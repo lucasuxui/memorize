ecd9adb026b0c01b5df5358c1b5e72e245462288
Lucas de Oliveira <lucasuxui@outlook.com>
2024-02-14 21:08:12 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Lucas de Oliveira Souza on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
