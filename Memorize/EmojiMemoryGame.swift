2ebe7431561073214330dacdf2ca7fa38e751df8
Lucas de Oliveira <lucasuxui@outlook.com>
2024-02-21 19:32:30 -0300

    Added ViewModel and Model

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Lucas de Oliveira Souza on 19/02/24.
+//
+
+import Foundation
---
+++ MemoryGame.swift
@@ -0,0 +1,24 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by Lucas de Oliveira Souza on 19/02/24.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+    
+}
