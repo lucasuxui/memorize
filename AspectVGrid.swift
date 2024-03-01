c070251eec4b2c98f867ceb005a01d575455f5bd
Lucas de Oliveira <lucasuxui@outlook.com>
2024-03-01 16:20:12 -0300

    AspectGrid file

---
+++ AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by Lucas de Oliveira Souza on 01/03/24.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
